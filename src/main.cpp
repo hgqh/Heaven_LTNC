7dc1dc1528d543408c33136301f9209f194d7054
“hgqh” <hgqh11@gmail.com>
2024-08-04 01:31:00 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,23 @@
+#include "bits/stdc++.h"
+#include "object.h"
 #include "game.h"
 #include "menu.h"
 
-Game* game = nullptr;
-Menu* menu = nullptr;
-
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-    menu = new Menu();
-    menu->init(renderer);
-
-    while (menu->isRunning()) {
-        menu->handleEvents();
-        menu->render();
-    }
-
-    delete menu;
-
-    game = new Game();
-    game->init("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, false);
-
-    while (game->running()) {
-        game->handleEvents();
-        game->update();
-        game->render();
-    }
-
-    game->clean();
-    delete game;
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
+SDL_Renderer *renderer;
+SDL_Window *window;
+TTF_Font *font;
+Game game;
+int main(int argc, char *args[])
+{
+     game.restart = 1;
+     while (game.restart)
+     {
+          game.variable();
+          game.serve();
+          game.runMenu();
+          if (game.run != 1)
+               game.rungame();
+          game.runback();
+          game.start = 0;
+     }
 }
