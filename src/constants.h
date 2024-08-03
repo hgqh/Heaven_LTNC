1f7352e6a705a65ae209fc9821147e9bfafdc100
“hgqh” <hgqh11@gmail.com>
2024-08-04 01:31:00 +0700

    Define constants

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int FONT_SIZE = 24;
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int SIZE = 16;
+const double PI = 3.14159265358979323846;
+const int BALL_SPEED = 5;
+const int SPEED = 5;
+
+#endif
