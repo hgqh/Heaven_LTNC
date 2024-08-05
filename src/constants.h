cad4f9b0582e27100153bd14ca7484c82b6d9fea
“hgqh” <hgqh11@gmail.com>
2024-08-05 22:31:00 +0700

    Change constants value

--- src/constants.h
+++ src/constants.h
@@ -1,12 +1,12 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int FONT_SIZE = 24;
-const int WIDTH = 800;
-const int HEIGHT = 600;
-const int SIZE = 16;
-const double PI = 3.14159265358979323846;
-const int BALL_SPEED = 5;
-const int SPEED = 5;
+const double SIZE= 17;
+const double BALL_SPEED= 16;
+const double SPEED = 12;
+const double PI = 3.14;
+const int WIDTH= 1000;
+const int HEIGHT= 600;
+const int FONT_SIZE= 56;
 
 #endif
